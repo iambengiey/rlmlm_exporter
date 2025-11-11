0188556c95dadf8f67c610f9d6e47b88eba23b27
Ben <36183043+iambengiey@users.noreply.github.com>
2025-11-11 13:17:22 +0200

    Update rlmlm_exporter.go

--- rlmlm_exporter.go
+++ rlmlm_exporter.go
@@ -25,7 +25,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/version"
-	"gopkg.in/alecthomas/kingpin.v2"
+	"github.com/alecthomas/kingpin/v2"
 )
 
 func init() {
