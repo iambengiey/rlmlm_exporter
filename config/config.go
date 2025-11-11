12271a5185e5413d507b72a790031a7919522559
Ben10 <ben1107@live.com>
2025-11-11 13:59:22 +0200

    updates

--- config/config.go
+++ config/config.go
@@ -1,17 +1,6 @@
 // Package config includes all individual types and functions to gather
 // the monitored licences.
-// (C) Copyright 2017 Mario Trangoni.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Licensed under the Apache License, Version 2.0.
 
 package config
 
@@ -29,7 +18,11 @@ import (
 var cfgLogger log.Logger = log.NewNopLogger()
 
 // SetLogger allows main to inject a real logger.
-func SetLogger(l log.Logger) { if l != nil { cfgLogger = l } }
+func SetLogger(l log.Logger) {
+	if l != nil {
+		cfgLogger = l
+	}
+}
 
 // ---------- YAML type definitions ----------
 
@@ -44,7 +37,7 @@ type License struct {
 	MonitorReservations bool   `yaml:"monitor_reservations"`
 }
 
-// Configuration type for all licences.
+// Configuration for all licences.
 type Config struct {
 	Licenses []License `yaml:"licenses"`
 }
@@ -54,7 +47,6 @@ func Load(path string) (*Config, error) {
 	if path == "" {
 		return nil, errors.New("config path is empty")
 	}
-
 	clean := filepath.Clean(path)
 	level.Info(cfgLogger).Log("msg", "loading config", "path", clean)
 
@@ -66,5 +58,15 @@ func Load(path string) (*Config, error) {
 
 	var cfg Config
 	if err := yaml.Unmarshal(data, &cfg); err != nil {
-		level.Error
-::contentReference[oaicite:0]{index=0}
+		level.Error(cfgLogger).Log("msg", "failed to parse YAML", "err", err)
+		return nil, err
+	}
+
+	level.Info(cfgLogger).Log("msg", "configuration loaded", "licenses", len(cfg.Licenses))
+	return &cfg, nil
+}
+
+// LoadConfig keeps backwards-compatibility with callers expecting this name.
+func LoadConfig(path string) (*Config, error) {
+	return Load(path)
+}
